0b2661cafb5fad4f79bbe31d455980c94abc2380
xFadedxShadow <63852447+xFadedxShadow@users.noreply.github.com>
2021-01-31 10:02:03 -0500

    Update Post-Installation.sh

--- Post-Installation.sh
+++ Post-Installation.sh
@@ -8,8 +8,8 @@ nano /etc/locale.conf
 nano /etc/hostname
 nano /etc/hosts
 passwd
-pacman -S grub efibootmgr networkmanager network-manager-applet dialog mtools dosfstools base-devel linux-zen-headers bluez bluez-utils alsa-utils pulseaudio pulseaudio-bluetooth git reflector xdg-utils xdg-user-dirs firefox-developer-edition
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=ARCH
+pacman -S grub efibootmgr networkmanager network-manager-applet dialog mtools dosfstools base-devel linux-zen-headers bluez bluez-utils alsa-utils pulseaudio pulseaudio-bluetooth git reflector xdg-utils xdg-user-dirs
+grub-install --target=x86_64-efi --efi-directory=/boot/efi
 grub-mkconfig -o /boot/grub/grub.cfg
 systemctl enable NetworkManager
 systemctl enable bluetooth
